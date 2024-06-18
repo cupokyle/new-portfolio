fa274e5b0e34aeb72711e1da31e05d6b4f74cfe0
cupokyle <kyleforsythcomedy@gmail.com>
2024-06-18 16:19:39 -0400

    Add skills grid

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 // src/components/About.js
 import React from 'react';
+import { DiJavascript1, DiReact, DiNodejsSmall, DiHtml5, DiCss3, DiPostgresql, DiDocker, DiGit, DiVisualstudio } from 'react-icons/di';
+import { SiCsharp, SiJenkins, SiJira, SiElasticsearch, SiRedis, SiPostman, SiVisualstudiocode } from 'react-icons/si'; // Import C#, Jenkins, Jira, Elasticsearch, Redis, SOAP (using VS Code as a placeholder), and RestAPI
 
 function About({ bioImage, bioAlt }) {
   const bio1 = "I'm Kyle, a passionate software developer with a love for video games. My journey into coding began with my interest in tabletop RPGs, which first got me fascinated with JavaScript.";
@@ -10,33 +12,48 @@ function About({ bioImage, bioAlt }) {
     <section id="about">
       <h2>About Me</h2>
       <div className='aboutBox'>
-      <div className='aboutHalf aboutHalfLeftTop'>
-        <div id='bioBox'>
-        <img className='bioHalf bioHalfLeft' src={bioImage} alt={bioAlt} />
-        <div className='bioHalf bioHalfRight'>
-          <h2>Kyle Forsyth</h2>
-          <br />
-          <h4>Profession:</h4>
-          <p>Software Applications Developer</p>
-          <br />
-          <h4>Recreation:</h4>
-          <p>Web App & Game Developer</p>
-          <br />
-          <h4>Bio:</h4>
-          <p>{bio1}</p>
-          <br />
-          <p>{bio2}</p>
-          <br />
-          <p>{bio3}</p>
+        <div className='aboutHalf aboutHalfLeftTop'>
+          <div id='bioBox'>
+            <img className='bioHalf bioHalfLeft' src={bioImage} alt={bioAlt} />
+            <div className='bioHalf bioHalfRight'>
+              <h2>Kyle Forsyth</h2>
+              <br />
+              <h4>Profession:</h4>
+              <p>Software Applications Developer</p>
+              <br />
+              <h4>Recreation:</h4>
+              <p>Web App & Game Developer</p>
+              <br />
+              <h4>Bio:</h4>
+              <p>{bio1}</p>
+              <br />
+              <p>{bio2}</p>
+              <br />
+              <p>{bio3}</p>
+            </div>
+          </div>
         </div>
+        <div className='aboutHalf aboutHalfRightBottom'>
+          <h2>Top Skills</h2>
+          <div id='skillGrid'>
+            <div className="skillItem"><DiReact size={70} /><span>React</span></div>
+            <div className="skillItem"><DiNodejsSmall size={70} /><span>Node.js</span></div>
+            <div className="skillItem"><DiJavascript1 size={70} /><span>JavaScript</span></div>
+            <div className="skillItem"><DiPostgresql size={70} /><span>PostgreSQL</span></div>
+            <div className="skillItem"><DiHtml5 size={70} /><span>HTML</span></div>
+            <div className="skillItem"><DiCss3 size={70} /><span>CSS</span></div>
+            <div className="skillItem"><DiDocker size={70} /><span>Docker</span></div>
+            <div className="skillItem"><DiGit size={70} /><span>Git</span></div>
+            <div className="skillItem"><DiVisualstudio size={70} /><span>Unity</span></div>
+            <div className="skillItem"><SiCsharp size={70} /><span>C#</span></div>
+            <div className="skillItem"><SiPostman size={70} /><span>RestAPI/SOAP</span></div>
+            <div className="skillItem"><SiElasticsearch size={70} /><span>Elasticsearch</span></div>
+            <div className="skillItem"><SiJira size={70} /><span>Jira</span></div>
+            <div className="skillItem"><SiRedis size={70} /><span>Redis</span></div>
+            <div className="skillItem"><SiJenkins size={70} /><span>Jenkins</span></div>
+            <div className="skillItem"><SiVisualstudiocode size={70} /><span>Visual Studio Code</span></div>
+          </div>
         </div>
-      </div>
-      <div className='aboutHalf aboutHalfRightBottom'>
-        <h2>Top Skills</h2>
-        <div id='skillGrid'>
-
-        </div>
-      </div>
       </div>
     </section>
   );
