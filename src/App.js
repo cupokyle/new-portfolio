8dfa3720d3fd378fdd16fd104a66d15c3c65f296
cupokyle <kyleforsythcomedy@gmail.com>
2024-06-18 15:45:53 -0400

    left half of about section first draft

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ import blackjackImage from './images/blackjack-screen.png';
 import whatNowImage from './images/wnow-screen.png';
 import tweeterImage from './images/tweeter-screen.png';
 import snipeImage from './images/snipe-screen.png';
+import bioImage from './images/bioImage.png';
 
 function App() {
+
+  // Bio data
+  const bioAlt='Photo of Kyle, a software developer, smiling and looking at the camera, with a professional and friendly demeanor.';
   // Sample project data
   const projects = [
-    { id: 1, title: 'Wedding Website', description: 'A Wedding Information Website - Created from scratch. Includes Embedded RSVP form', imageUrl: weddingImage, projectLink: 'https://beautifulwedding.netlify.app' },
+    { id: 1, title: 'Wedding Website', description: 'A Wedding Information Website - Created from scratch. Includes RSVP system powered by Cognito Forms', imageUrl: weddingImage, projectLink: 'https://beautifulwedding.netlify.app' },
     { id: 2, title: 'What Now?', description: 'A Choose-your-own-Adventure game the uses generative AI to progress the story', imageUrl: whatNowImage, projectLink: 'https://wnow.netlify.app/'  },
     { id: 3, title: 'Tweeter', description: 'A simple Twitter clone - Created as a React.js practise app to improve Front-end skills', imageUrl: tweeterImage, projectLink: 'https://walrus-app-pu4up.ondigitalocean.app/'  },
-    { id: 4, title: 'Rude Dude Blackjack', description: 'A Blackjack game that includes a really rude dude.', imageUrl: blackjackImage, projectLink: 'https://rudedudeblackjack.netlify.app/'  },
+    { id: 4, title: 'Rude Dude Blackjack', description: 'A basic Blackjack game made slightly more irritating by a very rude dude. Made with React.js', imageUrl: blackjackImage, projectLink: 'https://rudedudeblackjack.netlify.app/'  },
     { id: 5, title: 'Snipe Snacks', description: 'A game created in Unity - similar to Fruit Ninja', imageUrl: snipeImage, projectLink: 'https://play.unity.com/mg/other/snipe-snacks'  },
     { id: 6, title: 'A Picture of Two Cute Puppies', description: 'Just a picture I found online of some cute little puppies', imageUrl: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', projectLink: ''  },
   ];
@@ -35,7 +39,7 @@ function App() {
 </div>
 
         <ProjectList projects={projects} />
-        <About />
+        <About bioImage={bioImage} bioAlt={bioAlt}/>
         <Contact />
       </main>
       <Footer />
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,43 @@
 // src/components/About.js
 import React from 'react';
 
-function About() {
+function About({ bioImage, bioAlt }) {
+  const bio1 = "I'm Kyle, a passionate software developer with a love for video games. My journey into coding began with my interest in tabletop RPGs, which first got me fascinated with JavaScript.";
+  const bio2 = "Beyond software development, I have rich experience in producing comedy shows and scriptwriting. This blend of creativity and technical expertise defines my approach to projects.";
+  const bio3 = "I pride myself on being a creative and an 'Idea guy,' with a wealth of ever-growing ideas for projects I would like to create. When I'm not coding or gaming, you can find me diving into new tabletop adventures or crafting compelling scripts.";
+
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>Section coming soon</p>
+      <div className='aboutBox'>
+      <div className='aboutHalf aboutHalfLeftTop'>
+        <div id='bioBox'>
+        <img className='bioHalf bioHalfLeft' src={bioImage} alt={bioAlt} />
+        <div className='bioHalf bioHalfRight'>
+          <h2>Kyle Forsyth</h2>
+          <br />
+          <h4>Profession:</h4>
+          <p>Software Applications Developer</p>
+          <br />
+          <h4>Recreation:</h4>
+          <p>Web App & Game Developer</p>
+          <br />
+          <h4>Bio:</h4>
+          <p>{bio1}</p>
+          <br />
+          <p>{bio2}</p>
+          <br />
+          <p>{bio3}</p>
+        </div>
+        </div>
+      </div>
+      <div className='aboutHalf aboutHalfRightBottom'>
+        <h2>Top Skills</h2>
+        <div id='skillGrid'>
+
+        </div>
+      </div>
+      </div>
     </section>
   );
 }
